b2df8ded49b9ad5e984f37ff4e3aa2330a5c22c7
Thiago88pe <thiago_alves88@hotmail.com>
2025-03-09 22:24:23 -0300

    feat: implementa o navegador firefox

--- webdriver_factory.py
+++ webdriver_factory.py
@@ -6,11 +6,15 @@ from typing import Dict
 
 from dotenv import load_dotenv
 from selenium.common.exceptions import SessionNotCreatedException
-from selenium.webdriver import Chrome
+from selenium.webdriver import Chrome, Firefox
 from selenium.webdriver.chrome.options import Options as ChromeOptions
 from selenium.webdriver.chrome.service import Service as ChromeService
+from selenium.webdriver.firefox.options import Options as FirefoxOptions
+from selenium.webdriver.firefox.service import Service as FirefoxService
 from selenium.webdriver.remote.webdriver import WebDriver
 from webdriver_manager.chrome import ChromeDriverManager
+from webdriver_manager.firefox import GeckoDriverManager
+
 
 logging.basicConfig(level=logging.INFO, format="%(asctime)s - %(levelname)s - %(message)s")
 
@@ -81,11 +85,40 @@ class ChromeWebDriverOptions(WebDriverOptions):
         options.add_experimental_option(name="prefs", value=self.get_prefs())
         return options
 
+class FirefoxWebDriverOptions(WebDriverOptions):
+    """Configuração de opções e preferências do navegador Firefox."""
+    def get_prefs(self) -> Dict[str, object]:
+        """Retorna as preferências de download para o Firefox.
+
+        :return Dict[str, object]: Dicionário com as preferências de download do Firefox.
+        """
+        diretorio_download = os.getcwd()
+        prefs = {
+            "browser.download.folderList": 2,  # 2 = Usar diretório customizado
+            "browser.download.dir": diretorio_download,
+            "browser.helperApps.neverAsk.saveToDisk": "application/pdf, application/octet-stream",
+            "pdfjs.disabled": True,  # Abre PDF externamente
+        }
+        return prefs
+
+    def get_options(self) -> FirefoxOptions:
+        """Retorna as opções do Firefox.
+
+        :return ChromeOptions: Objeto de opções do Firefox.
+        """
+        options = FirefoxOptions()
+        if os.getenv(key="HEADLESS", default="false").lower() == "true":
+            options.add_argument(argument="--headless")
+
+        for key, value in self.get_prefs().items():
+            options.set_preference(name=key, value=value)
+        return options
+
 class WebDriverFactory:
     """Factory para criar WebDriver de diferentes navegadores."""
     _browsers = {
         "chrome": (Chrome, ChromeWebDriverOptions, ChromeService, ChromeDriverManager),
-
+        "firefox": (Firefox, FirefoxWebDriverOptions, FirefoxService, GeckoDriverManager),
     }
     def __init__(self) -> None:
         """Inicializa a fábrica de WebDriver, determinando o navegador a ser utilizado."""
